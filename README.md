ffea98de2fbf46fc9af815fd478057ad434a16d2
fmonthel <fmonthel@flox-arts.net>
2016-04-19 00:06:35 -0400

    [ENH] Args and AsciiTable

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # WekanStats
+
+Usage :
+
+	/parser.py --help
+	usage: parser.py [-h] [--action {list-stats,label-stats,user-stats}]
+    
+	Get some stats on Wekan Dashboard
+    
+	optional arguments:
+	  -h, --help            show this help message and exit
+	  --action {list-stats,label-stats,user-stats}
+
+
+To have stats based on Lists :
+
+	./parser.py --action list-stats
+	+-------------------------+--------------------+-----------------------+---------------+
+	| List name               | NB of live card(s) | NB of archive card(s) | Total card(s) |
+	+-------------------------+--------------------+-----------------------+---------------+
+	| Backlog                 |                 73 |                     2 |            75 |
+	| Backlog for the week    |                 30 |                     0 |            30 |
+	| In Progress             |                 18 |                     1 |            19 |
+	| Deploy (REL / INFRADAY) |                  5 |                     0 |             5 |
+	| Done                    |                  0 |                   139 |           139 |
+	| Blocked                 |                 27 |                     1 |            28 |
+	+-------------------------+--------------------+-----------------------+---------------+
+	| Total : 6 list(s)       |                153 |                   143 |           296 |
+	+-------------------------+--------------------+-----------------------+---------------+
+	
+
+To have stats based on Labels :
+
+	./parser.py --action label-stats
+	+---------------------+--------------------+-----------------------+---------------+
+	| Label name          | NB of live card(s) | NB of archive card(s) | Total card(s) |
+	+---------------------+--------------------+-----------------------+---------------+
+	| COS                 |                  2 |                     2 |             4 |
+	| July                |                  0 |                     0 |             0 |
+	| August              |                  1 |                     0 |             1 |
+	| Puppet              |                 17 |                     7 |            24 |
+	| UNIX                |                 86 |                    76 |           162 |
+	| ADM                 |                  5 |                     1 |             6 |
+	| June                |                  1 |                     0 |             1 |
+	| NAS                 |                 20 |                    24 |            44 |
+	| MNG                 |                 10 |                     8 |            18 |
+	| Infraday            |                  3 |                    10 |            13 |
+	| TOT                 |                 10 |                     2 |            12 |
+	| May                 |                  1 |                     0 |             1 |
+	| SAN                 |                  7 |                    12 |            19 |
+	| TO                  |                 87 |                    80 |           167 |
+	| April               |                  5 |                     1 |             6 |
+	| PAPAPAP             |                  1 |                     0 |             1 |
+	| DR                  |                  2 |                     4 |             6 |
+	| TOTOTOO             |                 37 |                    27 |            64 |
+	| TITIT               |                 23 |                    15 |            38 |
+	+---------------------+--------------------+-----------------------+---------------+
+	| Total : 19 label(s) |                318 |                   269 |           587 |
+	+---------------------+--------------------+-----------------------+---------------+
+
+To have stats based on Users :
+
+	+--------------------------+--------------------+-----------------------+---------------+
+	| Username                 | NB of live card(s) | NB of archive card(s) | Total card(s) |
+	+--------------------------+--------------------+-----------------------+---------------+
+	| jp.tevoila               |                 10 |                     8 |            18 |
+	| tototoot                 |                  1 |                     0 |             1 |
+	| tototooto.tototoot       |                 12 |                    17 |            29 |
+	| toitii.tototooto         |                 24 |                    33 |            57 |
+	| yogourt.totot            |                  0 |                     0 |             0 |
+	| mathieu.tototo           |                  0 |                     0 |             0 |
+	| tototoot                 |                 11 |                    15 |            26 |
+	| olivier.lesom            |                 18 |                    13 |            31 |
+	| remi.totot               |                  0 |                     0 |             0 |
+	| fmonthel                 |                 24 |                    28 |            52 |
+	| tottoto                  |                 45 |                    36 |            81 |
+	| titit.tototooto          |                  0 |                     0 |             0 |
+	+--------------------------+--------------------+-----------------------+---------------+
+	| Total : 12 User(s)       |                145 |                   150 |           295 |
+	+--------------------------+--------------------+-----------------------+---------------+
--- parser.py
+++ parser.py
@@ -1,17 +1,27 @@
 #! /usr/bin/python
 
 import json
+import argparse
+import urllib
 from pprint import pprint
 from terminaltables import AsciiTable
+import operator
 
-json_file='input.json'
 
-json_data=open(json_file)
-data = json.load(json_data)
-#pprint(data)
+# PARAMS
+wekan_export_url_json ='http://monwekan/export.json'
+
+# Options
+parser = argparse.ArgumentParser(description='Get some stats on Wekan Dashboard')
+parser.add_argument('--action', action='store', dest='action', choices=['list-stats', 'label-stats', 'user-stats'])
+args = parser.parse_args()
+
+# Load JSON
+file = urllib.urlopen(wekan_export_url_json)
+data = json.load(file.read())
 json_data.close()
-
 tmp = dict()
+
 # Loop on users
 tmp['users'] = dict()
 for us in data['users'] :
@@ -30,8 +40,6 @@ for li in data['lists'] :
     tmp['lists'][ li['_id'] ]['sort'] = li['sort']
     tmp['lists'][ li['_id'] ]['cards_live'] = list()
     tmp['lists'][ li['_id'] ]['cards_arch'] = list()
-sorted(tmp['lists'], key=lambda student: student[2])
-
 
 # Loop on labels
 tmp['labels'] = dict()
@@ -40,6 +48,7 @@ for la in data['labels'] :
     tmp['labels'][ la['_id'] ]['name'] = la['name']
     tmp['labels'][ la['_id'] ]['cards_live'] = list()
     tmp['labels'][ la['_id'] ]['cards_arch'] = list()
+
 
 # Loop on cards
 tmp['cards'] = dict()
@@ -52,6 +61,7 @@ for ca in data['cards'] :
         tmp['lists'][ ca['listId'] ]['cards_live'].append(ca['_id'])
     else :
         tmp['lists'][ ca['listId'] ]['cards_arch'].append(ca['_id'])
+    
     # Populate labels
     for lab in ca['labelIds'] :
         if ca['archived'] == False :
@@ -64,18 +74,96 @@ for ca in data['cards'] :
             tmp['users'][ mem ]['cards_live'].append(ca['_id'])
         else :
             tmp['users'][ mem ]['cards_arch'].append(ca['_id'])
-# Sum-up
-print "####### Stats for Wekan board : '" + data['title'] + "' #######"
-print "#### LISTS STATS ####"
-for li in tmp['lists_sort'].values() : 
-    print "- Number of cards in list '" + tmp['lists'][ li ]['name'] + "' : live => " + str(len(tmp['lists'][ li ]['cards_live'])) + " archive => " + str(len(tmp['lists'][ li ]['cards_arch']))
-print "#### LABEL STATS ####"
-for la in tmp['labels'].values() :
-    print "- Number of active cards in label '" + la['name'] + "' : live => " + str(len(la['cards_live'])) + " archive => " + str(len(la['cards_arch']))
-print "#### USER STATS ####"
-for us in tmp['users'].values() :
-    print "- Number of active cards for user'" + us['username'] + "' : live => " + str(len(us['cards_live'])) + " archive => " + str(len(us['cards_arch']))
 
-#pprint(tmp)
-#print "Dimension: ", data['cubes'][cube]['dim']
-#print "Measures:  ", data['cubes'][cube]['meas']
+if args.action == 'list-stats' :
+
+    # Ascii table for List
+    myAsciiTableList = [['List name','NB of live card(s)','NB of archive card(s)','Total card(s)']]
+    cards_live_total = 0
+    cards_arch_total = 0
+    cards_total = 0
+    for li in tmp['lists_sort'].values() :
+        cards_live_total = cards_live_total + len(tmp['lists'][ li ]['cards_live'])
+        cards_arch_total = cards_arch_total + len(tmp['lists'][ li ]['cards_arch'])
+        cards_total = cards_total + len(tmp['lists'][ li ]['cards_live']) + len(tmp['lists'][ li ]['cards_arch'])
+        tmpdata = list()
+        tmpdata.append(tmp['lists'][ li ]['name']) # ListName
+        tmpdata.append(str(len(tmp['lists'][ li ]['cards_live']))) # Live cards    
+        tmpdata.append(str(len(tmp['lists'][ li ]['cards_arch']))) # Archive cards
+        tmpdata.append(str(len(tmp['lists'][ li ]['cards_arch'])+len(tmp['lists'][ li ]['cards_live']))) # Total cards    
+        myAsciiTableList.append(tmpdata)
+    # Total for List
+    tmpdata = list()
+    tmpdata.append("Total : " + str(len(myAsciiTableList) - 1) + " list(s)")
+    tmpdata.append(str(cards_live_total))
+    tmpdata.append(str(cards_arch_total))
+    tmpdata.append(str(cards_total))
+    myAsciiTableList.append(tmpdata)
+    # Create AsciiTable for List
+    myTable = AsciiTable(myAsciiTableList)
+    myTable.inner_footing_row_border = True
+    myTable.justify_columns[1] = myTable.justify_columns[2] = myTable.justify_columns[3] = 'right'
+    # Output data
+    print myTable.table
+
+if args.action == 'label-stats' :
+
+    # Ascii table for Label
+    myAsciiTableLabel = [['Label name','NB of live card(s)','NB of archive card(s)','Total card(s)']]
+    cards_live_total = 0
+    cards_arch_total = 0
+    cards_total = 0
+    for la in tmp['labels'].values() :
+        cards_live_total = cards_live_total + len(la['cards_live'])
+        cards_arch_total = cards_arch_total + len(la['cards_arch'])
+        cards_total = cards_total + len(la['cards_live']) + len(la['cards_arch'])
+        tmpdata = list()
+        tmpdata.append(la['name']) # ListName
+        tmpdata.append(str(len(la['cards_live']))) # Live cards    
+        tmpdata.append(str(len(la['cards_arch']))) # Archive cards
+        tmpdata.append(str(len(la['cards_arch'])+len(la['cards_live']))) # Total cards    
+        myAsciiTableLabel.append(tmpdata)
+    # Total for Label
+    tmpdata = list()
+    tmpdata.append("Total : " + str(len(myAsciiTableLabel) - 1) + " label(s)")
+    tmpdata.append(str(cards_live_total))
+    tmpdata.append(str(cards_arch_total))
+    tmpdata.append(str(cards_total))
+    myAsciiTableLabel.append(tmpdata)
+    # Create AsciiTable for Label
+    myTable = AsciiTable(myAsciiTableLabel)
+    myTable.inner_footing_row_border = True
+    myTable.justify_columns[1] = myTable.justify_columns[2] = myTable.justify_columns[3] = 'right'
+    # Output data
+    print myTable.table
+
+if args.action == 'user-stats' :
+    
+    # Ascii table for User
+    myAsciiTableUser = [['Username','NB of live card(s)','NB of archive card(s)','Total card(s)']]
+    cards_live_total = 0
+    cards_arch_total = 0
+    cards_total = 0
+    for us in tmp['users'].values() :
+        cards_live_total = cards_live_total + len(us['cards_live'])
+        cards_arch_total = cards_arch_total + len(us['cards_arch'])
+        cards_total = cards_total + len(us['cards_live']) + len(us['cards_arch'])
+        tmpdata = list()
+        tmpdata.append(us['username']) # UserName
+        tmpdata.append(str(len(us['cards_live']))) # Live cards    
+        tmpdata.append(str(len(us['cards_arch']))) # Archive cards
+        tmpdata.append(str(len(us['cards_arch'])+len(us['cards_live']))) # Total cards    
+        myAsciiTableUser.append(tmpdata)
+    # Total for User
+    tmpdata = list()
+    tmpdata.append("Total : " + str(len(myAsciiTableUser) - 1) + " User(s)")
+    tmpdata.append(str(cards_live_total))
+    tmpdata.append(str(cards_arch_total))
+    tmpdata.append(str(cards_total))
+    myAsciiTableUser.append(tmpdata)
+    # Create AsciiTable for Label
+    myTable = AsciiTable(myAsciiTableUser)
+    myTable.inner_footing_row_border = True
+    myTable.justify_columns[1] = myTable.justify_columns[2] = myTable.justify_columns[3] = 'right'
+    # Output data
+    print myTable.table
